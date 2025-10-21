2d16c8e5e6933318c275cc9d865672811b9fc35c
Tobbie91 <oluwatobiakinrimisi@gmail.com>
2025-10-21 02:07:38 +0100

    docs: add Google Doc link to README

--- ng-host/src/main.ts
+++ ng-host/src/main.ts
@@ -1,11 +1,5 @@
-// import { bootstrapApplication } from '@angular/platform-browser';
-// import { appConfig } from './app/app.config';
-// import '@sbsc-assessment/react';
-// import App from 'next/app';
 
-
-// bootstrapApplication(App, appConfig).catch((err: any) => console.error(err));
-import '@sbsc-assessment/react';  // <-- defines <react-collab-widget>
+import '@sbsc-assessment/react'; 
 
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app';
